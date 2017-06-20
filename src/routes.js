5f55b7b536c79804e1d0af58abd4561bb2ce4149
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-06-20 15:26:21 +0300

    feat(Structure): Initial Components  - create login and home components  - setup react routes  - add css file
    [Delivers Task #002]

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import AuthPage from './components/auth/AuthPage';
+import HomePage from './components/home/HomePage';
+
+export default (
+  <Route path="/" component={App}>
+    <Route path="/auth/login/" component={AuthPage} />
+    <Route path="/home" component={HomePage} />
+  </Route>
+);
