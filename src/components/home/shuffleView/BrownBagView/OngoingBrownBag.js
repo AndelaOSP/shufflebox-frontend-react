82c73ab140e6324c105e8663ce5117ea348e4617
ishiundu <ian.shiundu@andela.com>
2017-06-29 17:32:37 +0300

    Add Ongoing Brown Bag Component

--- src/components/home/shuffleView/BrownBagView/OngoingBrownBag.js
+++ src/components/home/shuffleView/BrownBagView/OngoingBrownBag.js
@@ -13,16 +13,16 @@ class OngoingBrownBag extends React.Component {
         </div>
         <ul className="mdl-list">
           <li className="mdl-list__item mdl-list__item--two-line">
-          <span className="mdl-list__item-primary-content">
-            <img
-            className="avatar"
-            src="https://motherboard-images.vice.com//content-images/contentimage/41599/1485499779158756.jpg"
-            alt="user image not found"/>
-            <div className="user-info">
-              <span>Person One</span>
-            </div>
-          </span>
-        </li>
+            <span className="mdl-list__item-primary-content">
+              <img
+              className="avatar"
+              src="https://motherboard-images.vice.com//content-images/contentimage/41599/1485499779158756.jpg"
+              alt="user image not found"/>
+              <div className="user-info">
+                <span>Person One</span>
+              </div>
+            </span>
+          </li>
         </ul>
       </div>
     );
