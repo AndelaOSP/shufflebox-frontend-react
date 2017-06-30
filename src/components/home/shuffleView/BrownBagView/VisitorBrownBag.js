1a50aa233886e853c3cf30a4de3920f463ec2fc8
andela-gochieng <gertrude.ochieng@andela.com>
2017-06-30 16:01:15 +0300

    feat(visitor panel): modify presentation

--- src/components/home/shuffleView/BrownBagView/VisitorBrownBag.js
+++ src/components/home/shuffleView/BrownBagView/VisitorBrownBag.js
@@ -7,15 +7,15 @@ class VisitorBrownBag extends React.Component {
 
   render() {
     return (
-      <div className="onlist-brown-bag">
-        <div className="onlist-title">
-          <span>Visiting</span>
+      <div className="visitors-brown-bag">
+        <div className="visitors-title">
+          <span>VISITING</span>
         </div>
         <ul className="mdl-list">
           <li className="mdl-list__item mdl-list__item--two-line">
             <span className="mdl-list__item-primary-content">
               <img
-              className="avatar"
+              className="visitor-avatar"
               src="https://motherboard-images.vice.com//content-images/contentimage/41599/1485499779158756.jpg"
               alt="user image not found"/>
               <div className="user-info">
