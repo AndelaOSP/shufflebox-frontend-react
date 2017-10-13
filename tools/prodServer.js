7ae4dad0f8767948971ae1734145ba95142731e5
Boniface Mwenda <boniface.mwenda@andela.com>
2017-10-13 12:14:14 +0300

    [Fix] serve static folder on production

--- tools/prodServer.js
+++ tools/prodServer.js
@@ -7,7 +7,7 @@ import open from "open";
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
--- webpack.baseConfig.js
+++ webpack.baseConfig.js
@@ -26,10 +26,7 @@ module.exports =  {
     rules: [{
         test: /\.js$/,
         use: [{
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015','react', 'stage-1']
-          }
+          loader: 'babel-loader'
         }],
         exclude: /node_modules/
       },
