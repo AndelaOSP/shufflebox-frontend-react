0844b2e5fd4d4b3e311539ea305c6452587ac3ee
brayoh <bkuriakb@gmail.com>
2017-11-13 14:26:51 +0300

    Add logger

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,15 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 import webpack from "webpack";
 import path from "path";
 import open from "open";
 
-const config = require('../webpack.config.dev');
+const config = require('../webpack.config.dev.js');
 /* eslint-disable no-console */
 
 const port = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
 
+// app logger
+app.use(morgan("dev"));
 app.use(
   require("webpack-dev-middleware")(compiler, {
     noInfo: true,
